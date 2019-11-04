40cfabc0396f60829089bbe4eb35d0403c95b548
Fred K. Schott <fschott@ripple.com>
2019-11-03 22:20:42 -0800

    guided setup

---
+++ src/setup/index.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import c from 'ansi-colors';
+import Handlebars from 'handlebars';
+const { prompt } = require('enquirer');
+const cwd = process.cwd();
+
+(async function () {
+  console.log(`${c.green('✔')} Creating a new secret file...`);
+  const secretExampleFileLoc = path.join(cwd, '.secret_config-example.js');
+  const secretDestinationFileLoc = path.join(cwd, '.secret_config.js');
+  const secretFileTemplate = fs.readFileSync(secretExampleFileLoc, {encoding: 'utf8'});
+  fs.copyFileSync(secretExampleFileLoc, secretDestinationFileLoc);
+
+  // Use `process.stdout.write` instead of `console.log` because we don't want a newline added
+  process.stdout.write(`${c.green('✔')} Generating a new random API key... `);
+  const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  process.stdout.write(c.dim(randomString) + '\n');
+
+  const {serverUrl, accountAddress, accountSecret} = await prompt([{
+      type: 'input',
+      name: 'serverUrl',
+      message: 'Network Server Address',
+      initial: 'wss://s.altnet.rippletest.net:51233 (Testnet)',
+      result: (val: string) => val.trim().split(' ').shift(),
+      validate: (val: string) => val.trim().split(' ').shift()!.length > 0,
+    },
+    {
+      type: 'input',
+      name: 'accountAddress',
+      message: 'Account Public Address',
+      result: (val: string) => val.trim(),
+      validate: (val: string) => val.trim().length > 0
+    },
+    {
+      type: 'input',
+      name: 'accountSecret',
+      message: 'Account Private Key',
+      result: (val: string) => val.trim(),
+      validate: (val: string) => val.trim().length > 0
+    }
+  ]);
+
+  const secretTemplate = Handlebars.compile(secretFileTemplate);
+  const secretTemplated = secretTemplate({SERVER_ADDRESS_HERE: serverUrl, ACCOUNT_ADDRESS_HERE: accountAddress, RANDOM_STRING_HERE: randomString, ACCOUNT_SECRET_HERE: accountSecret});
+  fs.writeFileSync(secretDestinationFileLoc, secretTemplated, {encoding: 'utf8'});
+  console.log('✅ Setup complete! Start the server in dev mode: ' + c.bold('yarn dev'));
+})();
