0124ab5fe6cdc3fe816ccaba046f5a41ed343973
Elliot Lee <github.public@intelliot.com>
2020-04-03 01:55:04 -0700

    Use a js file for config
    Allows for comments and more flexibility

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,24 @@
-import fs from 'fs';
 let memoizedConfig: any = null;
 
 interface SecretConfig {
-  server: string,
+  server: string;
   accounts: {
     [index: string]: {
-      apiKey: string,
-      secret: string
-    }
-  }
+      apiKey: string;
+      secret: string;
+    };
+  };
 }
 
 export function getConfig(): SecretConfig {
   if (!memoizedConfig) {
     try {
-      const configStr = fs.readFileSync('.secret_config.json', {encoding: 'utf8'});
-      memoizedConfig = JSON.parse(configStr);
+      memoizedConfig = require('../.secret_config.js');
     } catch (err) {
-      console.error('Problem loading config file!\nMake sure that you run the "setup" command to generate a valid ".secret_config.json" file in your current working directory.');
+      console.error('Problem loading config file!\nMake sure that you run the "setup" command to generate a valid ".secret_config.js" file in your current working directory.');
       console.error(err);
       process.exit(1);
     }
   }
   return memoizedConfig;
 }
-  
