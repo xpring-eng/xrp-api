c86f74f565e479e8b84ffb6279c0244751cb1fc2
Elliot Lee <github.public@intelliot.com>
2020-05-19 00:29:03 -0700

    Use example config when running in CI

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,11 @@ let memoizedConfig: SecretConfig;
 export function getConfig(): SecretConfig {
   if (!memoizedConfig) {
     try {
-      memoizedConfig = require('../.secret_config.js');
+      if (process.env.IN_CI_ENVIRONMENT) {
+        memoizedConfig = require('../.secret_config-example.js');
+      } else {
+        memoizedConfig = require('../.secret_config.js');
+      }
     } catch (err) {
       console.error('Problem loading config file!\nMake sure that you run the "setup" command to generate a valid ".secret_config.js" file in your current working directory.');
       console.error(err);
